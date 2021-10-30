f4b8566207a22ccbe9b157515e7d171d6bb37680
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-10-30 11:59:57 -0600

    Redirect and Seesion

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+app.secret_key = "skittles"
 
 @app.route('/')
 def index():
@@ -8,8 +9,15 @@ def index():
 @app.route('/users', methods = ['POST'])
 def create_user():
     print("Got Post Info")
+    session['username'] = request.form['name']
+    session['useremail'] = request.form['email']
+    return redirect('/show')
+
+@app.route('/show')
+def show_user():
+    print("Showing the User Info From the Form")
     print(request.form)
-    return redirect('/')
+    return render_template("show.html", name_on_template=session['username'], email_on_template=session['useremail'])
 
 if __name__ == "__main__":
     app.run(debug=True)
